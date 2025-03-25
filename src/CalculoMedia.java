d68759cd1d28601a9b6d57073dd40282f934c9c4
Matheus Viersa <matheusviersa995@hotmail.com>
2025-03-25 13:58:37 -0300

    Tarefa finalizada

---
+++ src/CalculoMedia.java
@@ -0,0 +1,10 @@
+public class CalculoMedia {
+   public static void main(String[] args) {
+        float nota1=5;
+        float nota2=6;
+        float nota3=7;
+        float nota4=8;
+        float media=(nota1+nota2+nota3+nota4)/4;
+       System.out.print(media);
+   }
+}
